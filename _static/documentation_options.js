ef9f6aa642c41f6f740f0422ed2f5040e29d1c9b
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-07-02 14:36:44 +0000

    Update docs after building Travis build 15 of leilakhalili87/byxtal
    The docs were built from the branch 'master' against the commit
    6a979aedbee63871e3f1fb2ef82011cf92779fc8.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/leilakhalili87/byxtal/jobs/704348703.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post18+g27b8734',
+    VERSION: '0.post19+g6a979ae',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
