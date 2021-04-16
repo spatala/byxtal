3cb51bfebcbf55ba85c652fdf5af2d8720dfedb0
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2021-04-16 17:04:10 +0000

    Update docs after building Travis build 97 of spatala/byxtal
    The docs were built from the branch 'master' against the commit
    6f320a0576c61c7643483044199bfc39c8e109de.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/spatala/byxtal/jobs/767291929.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post61+g6f320a0',
+    VERSION: '0.post59+g6f320a0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
